05c6d7e2fcbebdc6086350d319fcd569d901192d
Shijie Chang <92910084+DUT-CSJ@users.noreply.github.com>
2025-10-20 19:05:58 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,22 @@
+import os
+
+# data path to be inferred
+amdsd_root_test =''
+btd_root_test =''
+polyp_root_test = ''
+ebhi_root_Test =''
+COVID_root_test = ''
+Breast_root_test = ''
+tnui_root_test = ''
+isic2018_root_test = ''
+
+
+amdsd = os.path.join(amdsd_root_test)
+btd =  os.path.join(btd_root_test)
+ebhi = os.path.join(ebhi_root_Test)
+tnui = os.path.join(tnui_root_test)
+polypfive = os.path.join(polyp_root_test)
+covid = os.path.join(COVID_root_test)
+breast = os.path.join(Breast_root_test)
+isic2018  = os.path.join(isic2018_root_test)
+
